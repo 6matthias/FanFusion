a4c33f3023b5b5e644b9bf467518d9029b7a6599
6matthias <matthiasfal@gmail.com>
2022-06-05 15:45:15 +0200

    First Commit

---
+++ assets/fusionGen.py
@@ -0,0 +1,14 @@
+from xml.etree.ElementTree import tostring
+from PIL import Image as im
+full = im.open("./full.png")
+
+size = full.size[0]
+print(size)
+mojiCount = 32
+
+mojiSize = size/mojiCount
+for y in range(mojiCount):
+    for x in range(mojiCount):
+        newCrop = full.crop((x*mojiSize, y*mojiSize, (x+1)*mojiSize, (y+1)*mojiSize))
+        newCrop.save("./fusions/"+str(x)+"_"+str(y)+".png")
+        print(str(mojiCount*y+x)+"/"+str(mojiCount*mojiCount))
---
+++ code.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+
+    function update() {
+
+        let one = $('#emo1').find(":selected").val();
+        let two = $('#emo2').find(":selected").val();
+
+        $(".result").attr("src", `assets/fusions/${one}_${two}.png`);
+
+    }
+
+    let mojicount = 32
+    const mojinames =
+        "Surprised;Blush;Confused;Crying;Dissapoint;Flush;Happy;Laugh;Putoff;Puzzled;Sad;Shocked;Sleep;Sweating;Sweat Sideye;Sweatsmile <3;Think;Troll;Upside Down;Wink;Star;Imp;Earth/Wiki;Coin;Heart/Love;Mind Blown;Yes;No;Sweat Think;Relief;Nope";
+
+    let names = mojinames.split(";")
+    for (var i = 1; i < mojicount; i++) {
+
+        let names = mojinames.split(";")
+
+        $('#emo1').append($('<option>', {
+            value: i,
+            text: names[i - 1]
+        }));
+
+        $('#emo2').append($('<option>', {
+            value: i,
+            text: names[i - 1]
+        }));
+
+    }
+
+    $('#emo1').change(function () {
+
+        let currSel = $('#emo1').find(":selected").val();
+        $(".emote1").attr("src", `assets/fusions/${currSel}_0.png`);
+        update()
+
+    })
+
+    $('#emo2').change(function () {
+
+        let currSel = $('#emo2').find(":selected").val();
+        $(".emote2").attr("src", `assets/fusions/${currSel}_0.png`);
+        update()
+
+    })
+
+    $(".emote1").attr("src", `assets/fusions/1_0.png`);
+    $(".emote2").attr("src", `assets/fusions/1_0.png`);
+    update();
+
+}) 
