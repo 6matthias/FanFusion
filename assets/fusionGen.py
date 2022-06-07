1afc1c4386adbcc4e81801f4bb276292ee58123a
6matthias <matthiasfal@gmail.com>
2022-06-07 23:14:56 +0200

    Negative space fix

--- assets/fusionGen.py
+++ assets/fusionGen.py
@@ -4,11 +4,11 @@ full = im.open("./full.png")
 
 size = full.size[0]
 print(size)
-mojiCount = 32
+mojiCount = 33
 
 mojiSize = size/mojiCount
 for y in range(mojiCount):
     for x in range(mojiCount):
-        newCrop = full.crop((x*mojiSize, y*mojiSize, (x+1)*mojiSize, (y+1)*mojiSize))
+        newCrop = full.crop((x*mojiSize, y*mojiSize, (x+1/9*8)*mojiSize, (y+1/9*8)*mojiSize))
         newCrop.save("./fusions/"+str(x)+"_"+str(y)+".png")
         print(str(mojiCount*y+x)+"/"+str(mojiCount*mojiCount))
